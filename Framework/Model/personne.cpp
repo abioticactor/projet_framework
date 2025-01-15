3ed28b02e0d111659449a720e85cd359ce4abb26
TomyR21 <tom.royer@reseau.eseo.fr>
2025-01-15 11:42:02 +0100

    Commentaire personne.cpp

--- Framework/Model/personne.cpp
+++ Framework/Model/personne.cpp
@@ -1,13 +1,17 @@
 #include "Personne.h"
 
+// Initialisation de la variable statique
 int Personne::identifiant = 0;
 
+
+// Constructeur
 Personne::Personne(std::string nom, std::string prenom) {
     this->nom = nom;
     this->prenom = prenom;
-    this->id = ++identifiant;
+    this->id = ++identifiant;// Génération automatique d'un identifiant unique
 }
 
+// Getters
 std::string Personne::getNom() const {
     return nom;
 }
@@ -20,6 +24,7 @@ std::string Personne::getPrenom() const {
     return prenom;
 }
 
+// Setters
 void Personne::setPrenom(std::string prenom) {
     this->prenom = prenom;
 }
