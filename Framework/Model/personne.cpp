2cc2e1f42842d5fd616ee79d82be23c9d69fe01f
abioticactor <107043904+abioticactor@users.noreply.github.com>
2025-01-15 11:40:46 +0100

    creation personne.cpp

--- Framework/Model/personne.cpp
+++ Framework/Model/personne.cpp
@@ -1,3 +1,29 @@
-#include "personne.h"
+#include "Personne.h"
 
-Personne::Personne() {}
+int Personne::identifiant = 0;
+
+Personne::Personne(std::string nom, std::string prenom) {
+    this->nom = nom;
+    this->prenom = prenom;
+    this->id = ++identifiant;
+}
+
+std::string Personne::getNom() const {
+    return nom;
+}
+
+void Personne::setNom(std::string nom) {
+    this->nom = nom;
+}
+
+std::string Personne::getPrenom() const {
+    return prenom;
+}
+
+void Personne::setPrenom(std::string prenom) {
+    this->prenom = prenom;
+}
+
+int Personne::getId() const {
+    return id;
+}
