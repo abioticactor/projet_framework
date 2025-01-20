a56d1013954e94138e803c31e44316e7cccd343b
PEMG39 <paulemile.martingrandvoinnet@reseau.eseo.fr>
2025-01-20 16:55:23 +0100

    Test TriCreneaux

--- Framework/Tests/testcalendrier.cpp
+++ Framework/Tests/testcalendrier.cpp
@@ -6,6 +6,7 @@ void TestCalendrier::runTests(){
     std::cout << "\nDEBUT des tests pour la Classe CALENDRIER\n " << std::endl;
     testAjouteRetireCreneau();
     testDispo();
+    testTri();
     std::cout << "\nFIN des tests pour la Classe CALENDRIER\n " << std::endl;
 }
 
@@ -50,10 +51,34 @@ void TestCalendrier::testDispo(){
         score+=1;
     }
     if (!c.estDispo(&creneau3)){
-        std::cout << "Test estDipoF: OK" << std::endl;
+        //std::cout << "Test estDipoF: OK" << std::endl;
         score+=1;
     }
     std::cout <<"Test Dispo: " << score << "/2" << std::endl;
 }
 
+void TestCalendrier::testTri(){
+    int score=0;
+    Creneau creneau1=Creneau("14/10/24","10:45");
+    Creneau creneau2=Creneau("14/10/24","11:45");
+    Creneau creneau3=Creneau("15/10/24","12:45");
+    Creneau creneau4=Creneau("16/10/24","09:45");
+    Calendrier c_ordo=Calendrier();
+    Calendrier c_desordo=Calendrier();
+    c_ordo.ajouteCreneau(&creneau1);
+    c_ordo.ajouteCreneau(&creneau2);
+    c_ordo.ajouteCreneau(&creneau3);
+    c_ordo.ajouteCreneau(&creneau4);
+    c_desordo.ajouteCreneau(&creneau1);
+    c_desordo.ajouteCreneau(&creneau3);
+    c_desordo.ajouteCreneau(&creneau4);
+    c_desordo.ajouteCreneau(&creneau2);
+    c_desordo.triCreneau();
+    for (int i=0;i<c_ordo.getTaille();i++){
+        if (c_ordo.getCalendrier()[i]==c_desordo.getCalendrier()[i]){
+            score+=1;
+        }
+    }
+    std::cout << "Test Tri: " << score << "/4" << std::endl;
+}
 
--- Framework/Tests/testcalendrier.h
+++ Framework/Tests/testcalendrier.h
@@ -11,6 +11,7 @@ public:
     static void runTests();
     static void testAjouteRetireCreneau();
     static void testDispo();
+    static void testTri();
 };
 
 #endif // TESTCALENDRIER_H
